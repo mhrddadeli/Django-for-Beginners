868d9c0d28c9234057dfcfc81ba9f31c06c8ac31
mhrddadeli <mhrddadeli@gmail.com>
2024-08-16 13:49:44 +0330

    added tests

--- message_board/posts/tests.py
+++ message_board/posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
-# Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.Post = Post.objects.create(text="salam test case!")
+        
+    def test_model_content(self):
+        self.assertEqual(self.Post.text, "salam test case!")
+
+    def test_url_exists_at_correct_location(self):
+        responce = self.client.get("/")
+        self.assertEqual(responce.status_code, 200)
+        
+    def test_hompage(self):
+        responce = self.client.get(reverse("home"))
+        self.assertEqual(responce.status_code, 200)
+        self.assertTemplateUsed(responce, "home.html")
+        self.assertContains(responce, "salam test case!")
