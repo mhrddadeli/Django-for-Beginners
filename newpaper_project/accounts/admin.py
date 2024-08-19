204654a43e5f9ab2b7896db35ee83acf58580374
mhrddadeli <mhrddadeli@gmail.com>
2024-08-19 18:56:21 +0330

    added user model and form

--- newpaper_project/accounts/admin.py
+++ newpaper_project/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('username', 'email', 'age', 'is_staff')
+    fieldsets = UserAdmin.fieldsets +((None, {'fields': ('age',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets +((None, {'fields': ('age',)}),)
+
+    """
+     The None value indicates that this new section does not have a title,
+     and {"fields": ("age",)} specifies that the age field should be included in this section.
+    """
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- newpaper_project/accounts/models.py
+++ newpaper_project/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+
+    # ChatGPT Generated Code
+    groups = models.ManyToManyField(
+        Group,
+        related_name='customuser_set',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='customuser_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
